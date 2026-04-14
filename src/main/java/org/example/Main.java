c2ea7557983a6312f1731d93713fc9f3b2f2ffc5
Hamit Yuvaci <yvchamit@gmail.com>
2026-04-13 22:12:08 -0500

    Java journey has started.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,41 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if(isBarking){
+            if (clock < 8 || clock >= 20) {
+                return true;
+            } else return false;
+        }
+
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        if(firstAge>=13 && firstAge <=19 || secondAge>=13 && secondAge <=19 || thirdAge>=13 && thirdAge <=19) return true;
+        return false;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        if (isSummer) {
+            if (temp >= 25 && temp <= 45) return true;
+            return false;
+        }
+
+        if (!isSummer) {
+            if (temp >= 25 && temp <= 35) return true;
+            return false;
+        }
+
+        return false;
     }
 
     public static double area(double width, double height) {
-       
+       if(width<0 || height<0) return -1;
+       return width * height;
     }
 
     public static double area(double radius) {
-       
+        if(radius<0) return -1;
+        return radius*radius*Math.PI;
     }
 }
